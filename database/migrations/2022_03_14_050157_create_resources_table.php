af2b8801b2f0e985549c3510ce547034baa27003
Derian Pinto <pintoderian@gmail.com>
2022-03-14 00:04:08 -0500

    Creando migraciones

---
+++ database/migrations/2022_03_14_050157_create_resources_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('person_id')->unsigned()->nullable();
+            $table->foreign('person_id')->references('id')->on('persons')->onDelete('cascade');
+            $table->string('facebook_link')->nullable();
+            $table->string('twitter_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+};
