0bff38838474a9d0aac6d5fd6e2d13d6793404fc
Derian Pinto <pintoderian@gmail.com>
2022-03-14 00:18:03 -0500

    Generando los seeder con los factory

---
+++ database/factories/ResourceFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Resource>
+ */
+class ResourceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'person_id' => rand(1, 20),
+            'facebook_link' => $this->faker->url(),
+            'twitter_link' => $this->faker->url(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Person::factory(20)->create();
+        \App\Models\Resource::factory(20)->create();
     }
 }
